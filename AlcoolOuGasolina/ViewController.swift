db4d17e6addeebca2997d5376013508966d3ac14
Raline <ralinesilva1@gmail.com>
2023-01-29 11:15:54 -0300

    commit inicial

---
+++ AlcoolOuGasolina/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlcoolOuGasolina
+//
+//  Created by Raline Maria da Silva on 29/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+
+
+}
+

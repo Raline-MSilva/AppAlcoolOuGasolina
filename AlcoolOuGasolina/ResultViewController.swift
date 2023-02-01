975afc3bc6e2b54a3dc9614882dc251bcb35eccc
Raline-MSilva <ralinesilva1@gmail.com>
2023-01-31 21:04:23 -0300

    criar controller do resultado

--- AlcoolOuGasolina/ResultViewController.swift
+++ AlcoolOuGasolina/ResultViewController.swift
@@ -5,4 +5,52 @@
 //  Created by Raline Maria da Silva on 29/01/23.
 //
 
-import Foundation
+import UIKit
+
+enum Choice: String {
+    case gasolina = "Gasolina"
+    case alcool = "Alcool"
+}
+
+class ResultViewController: UIViewController {
+    
+    var homeView: HomeView?
+    var result: ResultView?
+    let choice: Choice?
+    
+    init(choice: Choice) {
+        self.choice = choice
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        result = ResultView()
+        view = result
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkBackground
+        result?.resultLabel.text = choice?.rawValue
+        result?.delegate = self
+    }
+    
+    func popViewController() {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+extension ResultViewController: ResultViewDelegate {
+    func backButton() {
+        popViewController()
+    }
+    
+    func calculateButton() {
+        popViewController()
+    }
+
+}

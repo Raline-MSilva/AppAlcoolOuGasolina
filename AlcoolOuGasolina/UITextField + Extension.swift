4e2a82b3af162ac0074d31dd114aa53b4709c7c9
Raline-MSilva <ralinesilva1@gmail.com>
2023-01-31 21:05:38 -0300

    criar extesnion textField para abaixar o teclado

--- AlcoolOuGasolina/UITextField + Extension.swift
+++ AlcoolOuGasolina/UITextField + Extension.swift
@@ -5,4 +5,24 @@
 //  Created by Raline Maria da Silva on 31/01/23.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+    func addDoneButtonOnKeyboard() {
+        let dooneToobar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        dooneToobar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done = UIBarButtonItem(title: "Ok", style: .done, target: self, action: #selector(donneButtonAction))
+        
+        let items = [flexSpace, done]
+        dooneToobar.items = items
+        dooneToobar.sizeToFit()
+        self.inputAccessoryView = dooneToobar
+    }
+    
+    @objc func donneButtonAction() {
+        self.resignFirstResponder()
+    }
+}
+

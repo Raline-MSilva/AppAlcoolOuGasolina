a13c2f3f8dfe93f04975ca1e0de8fba207ce6233
Raline-MSilva <ralinesilva1@gmail.com>
2023-01-31 21:04:53 -0300

    criar extension para alterar a cor

--- AlcoolOuGasolina/UIColor + Extension.swift
+++ AlcoolOuGasolina/UIColor + Extension.swift
@@ -5,4 +5,10 @@
 //  Created by Raline Maria da Silva on 31/01/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static let lightBackground = UIColor(red: 217/255, green: 204/255, blue: 195/255, alpha: 85.0)
+    static let darkBackground = UIColor(red: 209/255, green: 160/255, blue: 138/255, alpha: 82.0)
+}
+
